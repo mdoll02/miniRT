98b5f87e879df537f878be03c87e759559be02e5
kx <kx@imkx.dev>
2023-05-02 18:37:54 +0200

    fix: file parser and object creator

--- src/parsing/rt_parser.c
+++ src/parsing/rt_parser.c
@@ -6,7 +6,7 @@
 /*   By: mdoll <mdoll@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 13:47:04 by mdoll             #+#    #+#             */
-/*   Updated: 2023/05/02 14:32:57 by mdoll            ###   ########.fr       */
+/*   Updated: 2023/05/02 18:30:13 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,118 +14,204 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "libft.h"
 
-static void	skip_spaces(char *line, int *i)
+static char	*skip_spaces(char *line)
 {
-	while (line[*i] == ' ' && line[*i])
-		(*i)++;
+	char	*cur;
+
+	cur = line;
+	while ((*cur == ' ' || *cur == '\t') && *cur)
+		cur++;
+	return (cur);
 }
 
-static int	set_type(char	*line, t_lexed_line *lexed_line, int *i)
+static char	*get_word_end(char *line)
 {
-	int		tmp;
-	int		char_count;
-	char	*type;
+	char	*cur;
 
-	char_count = 0;
-	skip_spaces(line, i);
-	tmp = *i;
-	while (ft_isalpha(line[*i]) && line[*i])
-	{
-		char_count++;
-		(*i)++;
-	}
-	if (char_count == 0 || char_count > 2)
-		return (1);
-	type = malloc(sizeof(char) * (char_count + 1));
-	if (!type)
-		return (1);
-	ft_strlcpy(type, &line[tmp], char_count + 1);
-	lexed_line->obj_name = type;
-	return (0);
+	cur = line;
+	while (*cur != ' ' && *cur != '\t' && *cur != '\n' && *cur)
+		cur++;
+	return (cur);
 }
 
-static int	allocate_array(t_lexed_line *lexed_line, char *line, int i)
+static char	*set_type(char	*line, t_lexed_line *lexed_line)
+{
+	char	*cur;
+
+	line = skip_spaces(line);
+	cur = get_word_end(line);
+	if (cur == line)
+		return (0);
+	lexed_line->obj_name = malloc(cur - line + 1);
+	if (!lexed_line->obj_name)
+		return (0);
+	ft_strlcpy(lexed_line->obj_name, line, cur - line + 1);
+	return (cur);
+}
+
+static int	allocate_flt_array(float **num_arr, char *line)
 {
 	int		nb_of_args;
 
 	nb_of_args = 0;
-	while (line[i] != '\n' && line[i])
+	while (*line && *line != '\n')
 	{
-		if (ft_isdigit(line[i]))
-		{
-			nb_of_args++;
-			while ((ft_isdigit(line[i]) || line[i] == '.') && line[i])
-				i++;
-		}
-		i++;
+		line = skip_spaces(line);
+		while ((ft_isdigit(*line) || *line == '.' || *line == '-') && *line)
+			line++;
+		if (*line == '/')
+			break ;
+		if (*line != ',' && *line != '\n' && skip_spaces(line) == line && *line)
+			return (0);
+		nb_of_args++;
+		line++;
 	}
-	lexed_line->values = malloc(sizeof(float) * nb_of_args);
-	if (!lexed_line->values)
+	if (!nb_of_args)
+		return (0);
+	*num_arr = malloc(sizeof(float) * nb_of_args);
+	if (!*num_arr)
+		return (0);
+	return (nb_of_args);
+}
+
+static int	lex_line(char *line, t_lexed_line *lexed_line)
+{
+	char	*cur;
+	char	*tmp;
+	int		i;
+
+	line = set_type(line, lexed_line);
+	if (!line)
 		return (1);
+	lexed_line->nb_of_values = allocate_flt_array(&lexed_line->values, line);
+	if (!lexed_line->nb_of_values)
+		return (2);
+	cur = line;
+	i = 0;
+	while (*cur && *cur != '\n')
+	{
+		cur = skip_spaces(cur);
+		line = cur;
+		if (*line == '/')
+			break ;
+		while ((ft_isdigit(*cur) || *cur == '.' || *cur == '-') && *cur)
+			cur++;
+		tmp = ft_substr(line, 0, cur - line);
+		lexed_line->values[i] = atof(tmp);
+		i++;
+		cur++;
+	}
 	return (0);
 }
 
-static t_lexed_line	lex_line(char *line)
+static ssize_t	get_file_size(char *file)
 {
-	t_lexed_line	lexed_line;
-	int				i;
-	int				nb_value;
-	int				value_len;
+	int		fd;
+	ssize_t	total_size;
+	ssize_t	size;
+	char	text[100];
 
-	i = 0;
-	set_type(line, &lexed_line, &i); // what should I return here in case of error?
-	skip_spaces(line, &i);
-	allocate_array(&lexed_line, line, i);
-	nb_value = 0;
-	while (line[i] != '\n')
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (1);
+	total_size = 0;
+	while (1)
 	{
-		if (ft_isdigit(line[i]))
-		{
-			value_len = i;
-			while ((ft_isdigit(line[value_len]) || line[value_len] == '.') && line[value_len])
-				value_len++;
-			lexed_line.values[nb_value] = (float)atof(ft_substr(line, i, value_len - i));
-			nb_value++;
-			while ((ft_isdigit(line[i]) || line[i] == '.') && line[i])
-				i++;
-		}
-		i++;
+		size = read(fd, &text, 100);
+		total_size += size;
+		if (size <= 0)
+			break ;
 	}
-	return (lexed_line);
+	close(fd);
+	return (total_size);
+}
+
+static void	append_object(t_object **objects, t_object *new_obj)
+{
+	t_object	*cur;
+
+	if (!*objects)
+	{
+		*objects = new_obj;
+		return ;
+	}
+	cur = *objects;
+	while (cur->next)
+		cur = cur->next;
+	cur->next = new_obj;
+}
+
+static int	add_object(t_lexed_line *lex, t_object **objects)
+{
+	t_any_object	*obj;
+
+	obj = malloc(sizeof(t_any_object));
+	if (!obj)
+		return (1);
+	if ((!ft_strcmp(lex->obj_name, "sp") && lex->nb_of_values != 7)
+		|| (!ft_strcmp(lex->obj_name, "pl") && lex->nb_of_values != 9)
+		|| (!ft_strcmp(lex->obj_name, "cy") && lex->nb_of_values != 11))
+	{
+		printf("Error: Wrong number of arguments for object %s\n",
+			lex->obj_name);
+		free(obj);
+		return (1);
+	}
+	ft_memcpy(obj + sizeof(t_object), lex->values,
+		sizeof(float) * lex->nb_of_values);
+	// TODO: add f_intersect and f_sample_color
+	append_object(objects, (t_object *)obj);
+	return (0);
+}
+
+static int	put_lexed_line(t_lexed_line *lex, t_world *world)
+{
+	if (!ft_strcmp(lex->obj_name, "A") && lex->nb_of_values == 4)
+		world->ambient = *(t_ambient *)lex->values;
+	else if (!ft_strcmp(lex->obj_name, "C") && lex->nb_of_values == 7)
+		world->camera = *(t_camera *)lex->values;
+	else if (!ft_strcmp(lex->obj_name, "L") && lex->nb_of_values == 7)
+		world->light = *(t_light *)lex->values;
+	else
+		return (add_object(lex, &world->objects));
+	return (0);
 }
 
 int	parse_rt_file(char	*file, t_world *world)
 {
 	t_lexed_line	lexed_line;
 	int				fd;
-	char			*file_to_lex;
+	char			*file_content;
+	ssize_t			file_size;
 	int				i;
 
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
 		return (1);
-	file_to_lex = malloc(sizeof(char) * 10000);
-	if (!file_to_lex)
-		return (1);
-	read(fd, &file_to_lex, 10000);
-	while (file_to_lex)
+	file_size = get_file_size(file);
+	if (file_size < 0)
+		return (2);
+	file_content = malloc(file_size + 1);
+	if (!file_content)
+		return (3);
+	read(fd, file_content, file_size);
+	i = 0;
+	while (file_content[i])
 	{
-		lexed_line = lex_line(file_to_lex);
-		// now you can use lexed_line
-		free(lexed_line.obj_name);
-		free(lexed_line.values);
-		i = 0;
-		while (file_to_lex[i])
-		{
-			if (file_to_lex[i] == '\n')
-				break ;
+		lex_line(file_content + i, &lexed_line);
+		put_lexed_line(&lexed_line, world);
+		if (lexed_line.obj_name)
+			free(lexed_line.obj_name);
+		if (lexed_line.values)
+			free(lexed_line.values);
+		while (file_content[i] != '\n' && file_content[i])
 			i++;
-		}
-		file_to_lex = &file_to_lex[i + 1];
+		if (file_content[i])
+			i++;
 	}
-	(void)world;
 	return (0);
 }
 
