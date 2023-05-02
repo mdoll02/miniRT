5f5a4fca263bf278b83d8e253468b5d8a76d18e6
kx <kx@imkx.dev>
2023-05-02 18:37:38 +0200

    fix: clean objects before application exists

--- src/hooks/close.c
+++ src/hooks/close.c
@@ -6,16 +6,29 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 10:46:53 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/05/02 11:29:50 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/05/02 18:35:38 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx/mlx.h>
-#include <libc.h>
+#include <stdlib.h>
 #include "hooks.h"
+
+static void	clean_objects(t_object *obj)
+{
+	t_object	*tmp;
+
+	while (obj)
+	{
+		tmp = obj;
+		obj = obj->next;
+		free(tmp);
+	}
+}
 
 int	close_hook(t_minirt *minirt)
 {
+	clean_objects(minirt->world.objects);
 	mlx_destroy_window(minirt->ctx, minirt->win);
 	free(minirt->ctx);
 	exit(0);
