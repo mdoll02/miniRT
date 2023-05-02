5fb8a0eb339841a0602b89a7eecf0e96221dd857
kx <kx@imkx.dev>
2023-05-02 23:31:38 +0200

    fix: added checkerboard sampler to rendering.h

--- includes/rendering.h
+++ includes/rendering.h
@@ -6,7 +6,7 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 20:39:12 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/05/02 23:06:57 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/05/02 23:26:39 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ t_vec3	plane_intersect(t_plane *plane, t_vec3 pos, t_vec3 dir);
 t_color	plane_sample_color(t_plane *plane, t_vec3 pos);
 t_vec3	cylinder_intersect(t_cylinder *cylinder, t_vec3 pos, t_vec3 dir);
 t_color	cylinder_sample_color(t_cylinder *cyl, t_vec3 pos);
+
+t_color	checkerboard_color(t_object *obj, t_vec3 pos);
 
 void	render_scene(t_minirt *minirt);
 
