d91b4b70d6da000adab7af5b39948ab19d556356
mdoll <mdoll@student.42wolfsburg.de>
2023-05-03 13:43:16 +0200

    fix: fixed checkerboard shader

--- src/math/checkerboard_sample.c
+++ src/math/checkerboard_sample.c
@@ -6,21 +6,32 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 23:27:04 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/05/02 23:27:04 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/05/03 11:16:09 by mdoll            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
 #include "rendering.h"
+
+#define CHECKER_SIZE 5.f
 
 t_color	checkerboard_color(t_object *obj, t_vec3 pos)
 {
-	t_color	check;
+	t_color	black;
+	t_color	white;
+	t_vec3	pos2;
+	t_vec3	ipos;
 
-	(void)obj;
-	check.r = (float)((int)(pos.x + 1000) % 2);
-	check.g = (float)((int)(pos.y + 1000) % 2);
-	check.b = (float)((int)(pos.z + 1000) % 2);
-	if (check.r == check.g && check.g == check.b)
-		return ((t_color){255, 255, 255});
-	return ((t_color){0, 0, 0});
+	(void) obj;
+	black = (t_color){0, 0, 0};
+	white = (t_color){255, 255, 255};
+	pos2.x = pos.x / CHECKER_SIZE;
+	pos2.y = pos.y / CHECKER_SIZE;
+	pos2.z = pos.z / CHECKER_SIZE;
+	ipos.x = roundf(pos2.x);
+	ipos.y = roundf(pos2.y);
+	ipos.z = roundf(pos2.z);
+	if ((int)(ipos.x + ipos.y + ipos.z) % 2 == 0)
+		return (white);
+	return (black);
 }
