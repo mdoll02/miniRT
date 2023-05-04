087d65ba805a69a441152ceaf63917db357a51a4
kx <kx@imkx.dev>
2023-05-04 23:07:20 +0200

    fix: somewhat fixed cylinder

--- src/math/cylinder_intersect.c
+++ src/math/cylinder_intersect.c
@@ -6,7 +6,7 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 23:06:28 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/05/04 12:33:36 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/05/04 22:32:52 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,53 +17,45 @@
 
 t_vec3 cylinder_intersect(t_cylinder *cylinder, t_vec3 pos, t_vec3 dir)
 {
-	double a = dir.x * dir.x + dir.z * dir.z;
-	double b = 2 * (dir.x * (pos.x - cylinder->pos.x) + dir.z * (pos.z - cylinder->pos.z));
-	double c = (pos.x - cylinder->pos.x) * (pos.x - cylinder->pos.x) + (pos.z - cylinder->pos.z) * (pos.z - cylinder->pos.z) - cylinder->radius * cylinder->radius;
-	double discriminant = b * b - 4 * a * c;
+	t_vec3 oc = vec3_sub(pos, cylinder->pos);
 
-	if (discriminant < 0)
-		return (vec3_init(NAN, NAN, NAN));
+	double a = vec3_dot(dir, dir) - pow(vec3_dot(dir, cylinder->axis), 2);
+	double b = 2 * (vec3_dot(dir, oc) - vec3_dot(dir, cylinder->axis) * vec3_dot(oc, cylinder->axis));
+	double c = vec3_dot(oc, oc) - pow(vec3_dot(oc, cylinder->axis), 2) - pow(cylinder->radius, 2);
 
-	double t1 = (-b - sqrt(discriminant)) / (2 * a);
-	double t2 = (-b + sqrt(discriminant)) / (2 * a);
+	double discr = b * b - 4 * a * c;
 
-	double y1 = pos.y + t1 * dir.y;
-	double y2 = pos.y + t2 * dir.y;
-	double y = cylinder->pos.y - cylinder->height / 2;
+	if (discr < 0)
+		return (t_vec3){INFINITY, INFINITY, INFINITY};
 
-	if (y1 < y || y1 > y + cylinder->height)
-		t1 = NAN;
+	double t1 = (-b + sqrt(discr)) / (2 * a);
+	double t2 = (-b - sqrt(discr)) / (2 * a);
 
-	if (y2 < y || y2 > y + cylinder->height)
-		t2 = NAN;
+	if (t1 > t2)
+	{
+		double temp = t1;
+		t1 = t2;
+		t2 = temp;
+	}
 
-	if (isnan(t1) && isnan(t2))
-		return (vec3_init(NAN, NAN, NAN));
+	t_vec3 point = vec3_add(pos, vec3_mul(dir, t1));
+	t_vec3 p1_to_p = vec3_sub(point, cylinder->pos);
+	double proj_len = vec3_dot(p1_to_p, cylinder->axis);
 
-	double t = isnan(t1) ? t2 : isnan(t2) ? t1 : fmin(t1, t2);
-	return (vec3_add(pos, vec3_mul(dir, t)));
+	if (proj_len < 0 || proj_len > cylinder->height)
+		return (t_vec3){INFINITY, INFINITY, INFINITY};
+
+	return point;
 }
 
 t_vec3 cylinder_normal(t_cylinder *cyl, t_vec3 pos)
 {
-	t_vec3 axis;
-	t_vec3 to_pos;
-	t_vec3 normal;
-	double d;
-
-	axis = vec3_normalize(cyl->axis);
-	to_pos = vec3_sub(pos, cyl->pos);
-	d = vec3_dot(to_pos, axis);
-	if (d < 0 || d > cyl->height)
-		return vec3_normalize(to_pos);
-	else
-	{
-		normal = vec3_normalize(vec3_sub(to_pos, vec3_mul(axis, d)));
-		if (vec3_dot(normal, to_pos) > 0)
-			normal = vec3_neg(normal);
-		return normal;
-	}
+	t_vec3 axis = vec3_normalize(cyl->axis);
+	t_vec3 to_pos = vec3_sub(pos, cyl->pos);
+	double proj_len = vec3_dot(to_pos, axis);
+	t_vec3 proj_vec = vec3_add(cyl->pos, vec3_mul(axis, proj_len));
+	t_vec3 normal = vec3_normalize(vec3_sub(pos, proj_vec));
+	return normal;
 }
 
 
