52bdd682ff9cda3fdcf88050defd5a2958724179
kx <kx@imkx.dev>
2023-05-04 23:06:59 +0200

    fix: clamp color

--- includes/color_math.h
+++ includes/color_math.h
@@ -6,7 +6,7 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 12:51:48 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/05/04 12:54:48 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/05/04 21:50:08 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include "definitions.h"
 
+t_color color_limit(t_color color);
 t_color	color_add(t_color a, t_color b);
 t_color color_scale(t_color color, double scalar);
 
--- src/math/color.c
+++ src/math/color.c
@@ -6,20 +6,28 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 12:52:15 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/05/04 12:54:31 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/05/04 21:49:57 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
 #include "color_math.h"
+
+t_color color_limit(t_color color)
+{
+	return ((t_color){fmax(fmin(color.r, 255), 0),
+					  fmax(fmin(color.g, 255), 0),
+					  fmax(fmin(color.b, 255), 0)});
+}
 
 t_color	color_add(t_color a, t_color b)
 {
-	return ((t_color){a.r + b.r, a.g + b.g, a.b + b.b});
+	return (color_limit((t_color){a.r + b.r, a.g + b.g, a.b + b.b}));
 }
 
 t_color color_scale(t_color color, double scaler)
 {
-	return ((t_color){color.r * scaler,
+	return (color_limit((t_color){color.r * scaler,
 					  color.g * scaler,
-					  color.b * scaler});
+					  color.b * scaler}));
 }
